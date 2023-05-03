9816c167b133b896b4e40fecdf595728764b95ee
Charles Dupont <charles.aug.dupont@gmail.com>
2023-05-03 12:33:31 +0200

    rename, refactor

--- model.py
+++ model.py
@@ -2,19 +2,19 @@ import numpy as np
 from tqdm import tqdm
 
 import scipy.integrate as integrate
-from scipy.optimize import minimize
-from scipy.stats import skew
+from scipy.optimize import minimize, root_scalar
 
 #################################################################################################
 
 # UTILITY FUNCTIONS
-def u_x(x, gamma=2.1, A=1.2):
+def U(x, gamma=2.1, A=1.2):
 	"""
 	Utility function for consumption of agent.
 	"""
 	return A*x**(1-gamma) / (1-gamma)
 
-def v_x(x, gamma=2.1):
+
+def V(x, gamma=2.1):
 	"""
 	Utility function for bequest of agent to offspring.
 	"""
@@ -23,18 +23,18 @@ def v_x(x, gamma=2.1):
 #################################################################################################
 
 # SAFE STRATEGY 
-def bequest_safe(w, c, r_hat):
+def bequest_safe(s, r_hat):
 	"""
 	Bequest when using safe strategy.
 	"""
-	return (w-c)*r_hat
+	return s*r_hat
 
 def consumption_utility(s, w, r_hat):
 	"""
 	Optimization function for safe investment strategy.
 	We seek optimal s value.
 	"""
-	return -(u_x(w-s) + v_x(s*r_hat))
+	return -(U(w-s) + V(s*r_hat))
 
 def no_project_best_utility(w, r_hat):
 	"""
@@ -52,75 +52,96 @@ def no_project_best_utility(w, r_hat):
 
 # RISKY STRATEGY
 def bequest_project(s, beta, r, r_hat, r_bar, I):
+	"""
+    Computes realized value of bequest for risky strategy with return r.
+    Args:
+        s    : amount saved
+        beta : optimal beta
+        r    : realized value of random variable for risky return
+    Returns:
+        the bequest amount
+    """
 	return (s - I)*r_hat + (1-beta)*I*r_bar + beta*I*r
 
-def B_r(s, beta, r, r_hat, r_bar, I):
-	return s*r_hat + I*(r_bar-r_hat) + I*beta*(r-r_bar)
-
-def B_0(s, beta, r0, r_hat, r_bar, I):
-	 return s*r_hat + I*(r_bar-r_hat) + I*beta*(r0-r_bar)
-
-def optimal_beta_integral(s, beta, r0, r1, r_bar, r_hat, e_bar, I):
-	return (1/(r1-r0)) * integrate.quad(lambda r : v_x(B_r(s, beta, r, r_hat, r_bar, I)), r0, r1)[0] - v_x(B_0(s, beta, r0, r_hat, r_bar, I)) - e_bar
-
-def find_optimal_beta(s, r0, r1, r_bar, r_hat, e_bar, I, lower=0, upper=1, tolerance=1e-9, max_iters=80):
+def optimal_beta_integral(beta, s, r0, r1, r_bar, r_hat, e_bar, I):
 	"""
-	Binary search to find zero of optimal_beta_integral function.
+    See Proposition 5.1
+    Args:
+        beta : beta value
+        s    : amount saved
+    Returns:
+        IC'
+    """
+	return (1/(r1-r0)) * integrate.quad(lambda r : V(bequest_project(s, beta, r, r_hat, r_bar, I)), r0, r1)[0] - \
+												   V(bequest_project(s, beta, r0, r_hat, r_bar, I)) - e_bar
+
+def find_optimal_beta(s, r0, r1, r_bar, r_hat, e_bar, I):
 	"""
-	iteration = 0
+    Function to find zero of optimal_beta_integral function, which corresponds to optimal beta.
+    Args:
+        s : amount saved
+    Returns:
+        optimal beta value; -1 if it does not exist
+    """
+	s = float(s)
+	upper = 1.0
 	while True:
-		mid = (lower+upper)/2
 		try:
-			result = optimal_beta_integral(s, mid, r0, r1, r_bar, r_hat, e_bar, I)
+			result = root_scalar(optimal_beta_integral, args=(s, r0, r1, r_bar, r_hat, e_bar, I), bracket=(0,upper)).root
+			assert not isinstance(result, complex)
+			return result
 		except:
-			upper = 0.5
-			continue
-		if abs(result) < tolerance:
-			break
-		if result < 0:
-			lower = mid
-		elif result > 0:
-			upper = mid
-		iteration += 1
-		if iteration > max_iters:
-			return -1
-	return mid
+			upper -= 0.1
+			if upper < 0:
+				return -1
+			else:
+				continue
 
 def project_optimization(s, w, r0, r1, r_bar, r_hat, e_bar, I):
-	beta = find_optimal_beta(float(s), r0, r1, r_bar, r_hat, e_bar, I)
-	B0 = B_0(s, beta, r0, r_hat, r_bar, I)
-	return -(u_x(w-s) + v_x(B0))
-
-def find_upper_s_bound(r0, r1, r_bar, r_hat, e_bar, I, lower=0, upper=10, tolerance=1e-9):
-	while True:
-		mid = (lower+upper)/2
-		b = find_optimal_beta(mid, r0, r1, r_bar, r_hat, e_bar, I)
-		if abs(b-1) < tolerance:
-			return mid
-		elif b == -1:
-			upper = mid
-		else:
-			lower = mid
-
-def project_best_utility(w, r0, r1, r_bar, r_hat, e_bar, I, upper_s_bound):
 	"""
-	Minimize project_optimization by finding optimal s.
-	Optimal beta is only defined for s in (0.0, upper_s_bound), 
-	so we set the bounds to (0, min(w, upper_s_bound)) since s <= w.
+    Optimization function for the risky investment strategy.
+    Args:
+        s: amount saved
+        w: wealth
+    Returns:
+        utility
+    """
+	beta = find_optimal_beta(s, r0, r1, r_bar, r_hat, e_bar, I)
+	B0 = bequest_project(s, beta, r0, r_hat, r_bar, I)
+	return -(U(w-s) + V(B0))
+
+def project_best_utility(w, r0, r1, r_bar, r_hat, e_bar, I):
 	"""
+    Minimize project_optimization by finding optimal s.
+    Args:
+        w: wealth 
+    Returns:
+        optimal utility, optimal s, optimal beta
+    """
 	# optimization parameter is s
-	result = minimize(project_optimization, args=(w, r0, r1, r_bar, r_hat, e_bar, I), bounds=((0,min(w, upper_s_bound)),), x0 = w/2)
+	result = minimize(project_optimization, args=(w, r0, r1, r_bar, r_hat, e_bar, I), bounds=((0,w),), method="L-BFGS-B", x0=w/2)
 	s = result.x[0]
 	utility = result.fun[0] 
-	return -utility, s, find_optimal_beta(float(s), r0, r1, r_bar, r_hat, e_bar, I)
+	return -utility, s, find_optimal_beta(s, r0, r1, r_bar, r_hat, e_bar, I)
 
 #################################################################################################
 
 # SIMULATIONS
-def simulate_lineage_wealth(w_init, r0, r1, r_bar,r_hat, e_bar, I, upper_s_bound, reps):
-
-	print(f"Running for I = {I}, e_bar = {e_bar}, r1={r1}...")
-
+def simulate_lineage_wealth(w_init, r0, r1, r_bar, r_hat, e_bar, I, reps):
+	"""
+	Simulate a lineage's wealth for reps periods.
+	Args:
+		w_init : initial wealth
+		r0     : left bound of uniform random project return
+		r1     : right bound ''
+		r_bar  : expected project return
+		r_hat  : safe return
+		e_bar  : entrepreneurial effort needed to undertake project
+		I      : capital needed to kickstart a project
+		reps   : number of periods to run simulation for
+	Returns:
+		list of lineage wealth values over time
+	"""
 	random_vals = np.random.uniform(r0, r1, size=reps)
 	lineage_wealth = np.zeros((reps,))
 	w = w_init
@@ -133,16 +154,15 @@ def simulate_lineage_wealth(w_init, r0, r1, r_bar,r_hat, e_bar, I, upper_s_bound
 
 		# speedup
 		if w <= max_wealth_project:
-			_, s_project, beta = project_best_utility(w, r0, r1, r_bar, r_hat, e_bar, I, upper_s_bound)
+			_, s_project, beta = project_best_utility(w, r0, r1, r_bar, r_hat, e_bar, I)
 			w = bequest_project(s_project, beta, random_vals[rep_idx], r_hat, r_bar, I)
 		elif w >= min_wealth_no_project:
 			_, s_no_project = no_project_best_utility(w, r_hat)
-			c = w - s_no_project
-			w = bequest_safe(w, c, r_hat)
+			w = bequest_safe(s_no_project, r_hat)
 
 		else:
 			utility_no_project, s_no_project = no_project_best_utility(w, r_hat)
-			utility_project, s_project, beta = project_best_utility(w, r0, r1, r_bar, r_hat, e_bar, I, upper_s_bound)
+			utility_project, s_project, beta = project_best_utility(w, r0, r1, r_bar, r_hat, e_bar, I)
 			# choose project
 			if utility_project > utility_no_project:
 				max_wealth_project = max(w, max_wealth_project)
@@ -150,8 +170,7 @@ def simulate_lineage_wealth(w_init, r0, r1, r_bar,r_hat, e_bar, I, upper_s_bound
 			# choose safe strategy
 			else:
 				min_wealth_no_project = min(w, min_wealth_no_project)
-				c = w - s_no_project
-				w = bequest_safe(w, c, r_hat)
+				w = bequest_safe(s_no_project, r_hat)
 
 		lineage_wealth[rep_idx] = w
 
