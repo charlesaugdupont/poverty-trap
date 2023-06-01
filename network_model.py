1d5948f14b627962a8043924031374e4ab4325cd
Charles Dupont <charles.aug.dupont@gmail.com>
2023-06-01 11:56:21 +0200

    cumulative prospect theory refactoring

--- network_model.py
+++ network_model.py
@@ -1,11 +1,14 @@
+import argparse
 import numpy as np
-import networkx as nx
 from tqdm import tqdm
+import networkx as nx
 from model_banerjee import U,V
+from multiprocessing import Pool
 from scipy.stats import gengamma
 from scipy.optimize import minimize
 
-from multiprocessing import Pool
+from cptopt.optimizer import MeanVarianceFrontierOptimizer, ConvexConcaveOptimizer
+from cptopt.utility import CPTUtility
 
 
 #################################################################################################
@@ -13,7 +16,6 @@ from multiprocessing import Pool
 # Network Construction
 
 def build_graph(n, graph_type, graph_args):
-
 	if graph_type == "powerlaw_cluster":
 		G = nx.powerlaw_cluster_graph(n=n, **graph_args) 
 	elif graph_type == "random_regular_graph":
@@ -78,7 +80,7 @@ def get_gamble_returns(P, size):
 
 # Optimization
 
-def utility(x, w, project_allocations, project_returns, risk, safe_return, A, gamma):
+def utility(x, w, investment_returns, A, gamma):
 	"""
 	Compute expected utility.
 	Args:
@@ -86,8 +88,6 @@ def utility(x, w, project_allocations, project_returns, risk, safe_return, A, ga
 		w                   : wealth level of agent
 		project_allocations : allocation of savings to risky projects
 		project_returns     : expected project return
-		risk                : parameter controlling level of risk-aversion
-		safe_return         : safe return amount
 		A                   : utility function parameter
 		gamma               : utility function parameter
 	Returns:
@@ -95,10 +95,9 @@ def utility(x, w, project_allocations, project_returns, risk, safe_return, A, ga
 	"""
 	# utility from consumption
 	consumption_utility = U(w*x, A=A, gamma=gamma)
-	
+
 	# expected utility from projects
-	i = 1-x
-	project_utility = V(w*i*(1-risk)*safe_return + sum(w*i*risk*project_allocations*project_returns))
+	project_utility = V(sum(w*(1-x)*investment_returns))
 	
 	return - (consumption_utility + project_utility)
 
@@ -117,24 +116,25 @@ def optimize_utility(arg_tuple):
 # Simulation
 
 def simulation(NUM_AGENTS=500, STEPS=50, SAFE_RETURN=1.1, DEFAULT_A=1.2, DEFAULT_GAMMA=2.1,
-			   PROJECT_COST=3.0, W0=0.8, W1=1.2, R0=0.3, R1=0.7, graph=None,
-			   graph_type="powerlaw_cluster", graph_args={"m":2, "p":0.5}):
+			   PROJECT_COST=3.0, W0=0.8, W1=1.2, GAMMA_POS_L=3, GAMMA_POS_R=9, graph=None,
+			   NUM_GAMBLE_SAMPLES=200000, graph_type="powerlaw_cluster", graph_args={"m":2, "p":0.5}):
 	"""
 	Runs ABM model.
 	Args:
-		NUM_AGENTS    : number of agents
-		STEPS         : number of steps
-		SAFE_RETURN   : safe return coefficient (> 1.0)
-		DEFAULT_A     : parameter used in utility functions
-		DEFAULT_GAMMA : parameter used in utility functions
-		PROJECT_COST  : minimum cost for project to be undertaken
-		W0            : left bound for uniform random wealth initialization
-		W1            : right bound for uniform random wealth initialization
-		R0            : left bound for uniform random risk initialization
-		R1            : right bound for uniform random risk initialization
-		graph 		  : NetworkX graph
-		graph_type    : type of graph to use
-		graph_args    : arguments for graph construction, specific to graph type passed
+		NUM_AGENTS    	   : number of agents
+		STEPS         	   : number of steps
+		SAFE_RETURN   	   : safe return coefficient (> 1.0)
+		DEFAULT_A     	   : parameter used in utility functions
+		DEFAULT_GAMMA 	   : parameter used in utility functions
+		PROJECT_COST  	   : minimum cost for project to be undertaken
+		W0            	   : left bound for uniform random wealth initialization
+		W1            	   : right bound for uniform random wealth initialization
+		GAMMA_POS_L	  	   : left bound for uniform random risk initialization
+		GAMMA_POS_R		   : right bound for uniform random risk initialization
+		NUM_GAMBLE_SAMPLES : number of random samples for cumulative prospect theory utility
+		graph 		  	   : NetworkX graph
+		graph_type    	   : type of graph to use
+		graph_args    	   : arguments for graph construction, specific to graph type passed
 	Returns:
 		WEALTH      : (STEPS, NUM_AGENTS) array containing wealth levels of agents at each iteration
 		communities : dict from community ID to list of members
@@ -159,63 +159,65 @@ def simulation(NUM_AGENTS=500, STEPS=50, SAFE_RETURN=1.1, DEFAULT_A=1.2, DEFAULT
 
 	# global attributes
 	GAMBLES = generate_gambles(len(communities))
+	GAMBLE_SAMPLES = np.zeros((NUM_GAMBLE_SAMPLES, len(GAMBLES)))
+	for i,g in enumerate(GAMBLES):
+		GAMBLE_SAMPLES[:,i] = np.random.choice(g["outcomes"], NUM_GAMBLE_SAMPLES, p=g["probs"]) - 1
 	GAMBLE_RETURNS = np.row_stack([[get_gamble_returns(P, size=STEPS) for P in GAMBLES]])
 	gamble_success   = np.zeros((len(GAMBLES)))
 	gamble_averages  = np.zeros((len(GAMBLES)))
-	gamble_variances = np.zeros((len(GAMBLES)))
-	# compute expected value and variance of each gamble
+	# compute expected value of each gamble
 	for i,g in enumerate(GAMBLES):
 		gamble_averages[i]  = np.average(g["outcomes"], weights=g["probs"])
-		gamble_variances[i] = np.average((g["outcomes"]-gamble_averages[i])**2, weights=g["probs"])
 			
 	# agent attributes
-	INCOME = np.zeros((STEPS, NUM_AGENTS))
-	WEALTH = np.random.uniform(W0, W1, (STEPS+1, NUM_AGENTS))
-	RISK   = np.random.uniform(R0, R1, NUM_AGENTS)
-	C      = np.zeros((NUM_AGENTS))
-	ALLOC  = []
+	INCOME	  = np.zeros((STEPS, NUM_AGENTS))
+	WEALTH 	  = np.random.uniform(W0, W1, (STEPS+1, NUM_AGENTS))
+	GAMMA_POS = np.random.uniform(GAMMA_POS_L, GAMMA_POS_R, NUM_AGENTS)
+	UTILITIES = [CPTUtility(gamma_pos=GAMMA_POS[i], gamma_neg=10, delta_pos=0.77, delta_neg=0.79) for i in range(NUM_AGENTS)]
+	C      	  = np.zeros((NUM_AGENTS))
+	AGENT_GAMBLE_AVERAGES = [np.concatenate([gamble_averages[community_membership[i]], [SAFE_RETURN]]) for i in range(NUM_AGENTS)]
 	
+	ALLOC = []
+	AGENT_EXPECTED_RETURNS = []
 	# compute optimal portfolios for agents
-	for i in range(NUM_AGENTS):
-		mu      = gamble_averages[community_membership[i]]
-		cov     = np.diag(gamble_variances[community_membership[i]])
-		cov_inv = np.linalg.inv(cov)
-		ones    = np.ones((len(mu),))
-		val     = cov_inv @ ((mu - SAFE_RETURN) * ones)
-		val     = val / (ones @ val)
-		ALLOC.append(val)
+	print("Computing optimal portfolios...")
+	for i in tqdm(range(NUM_AGENTS)):
+		mv = MeanVarianceFrontierOptimizer(UTILITIES[i])
+		samples = GAMBLE_SAMPLES[:,community_membership[i]]
+		samples_with_safe_gamble = np.column_stack([samples, np.repeat(SAFE_RETURN-1, samples.shape[0])])
+		mv.optimize(samples_with_safe_gamble)
+		ALLOC.append(mv.weights)
+		# pre-compute expected returns - note: if the mean variance optimizer is re-run, this needs to be updated!
+		AGENT_EXPECTED_RETURNS.append(ALLOC[-1]*AGENT_GAMBLE_AVERAGES[i])
 
 	# simulation
+	print("Performing time stepping...")
 	for step in tqdm(range(STEPS)):
 
 		project_contributions = np.zeros((len(GAMBLES)))
 
 		# all agents perform optimization step and we sum up project contributions
 
-		if multiprocess:
-			args = [(WEALTH[step][i], ALLOC[i], 
-					gamble_averages[community_membership[i]],
-					RISK[i], DEFAULT_A, DEFAULT_GAMMA, SAFE_RETURN)
-					for i in range(NUM_AGENTS)]
-			with Pool() as pool:
-				results = pool.map(optimize_utility, args)
-			C = np.array(results)
-			for i in range(NUM_AGENTS):
-				project_contributions[community_membership[i]] += \
-									np.sum(WEALTH[step][i]*ALLOC[i]*RISK[i]*(1-C[i]))  
-		else:
-			# serial approach (better than multiprocessing for O(10^3) agents, but worse for >= O(10^4))
-			for i in range(NUM_AGENTS):
-				C[i] = minimize(utility, x0=0.5, bounds=[(0.05, 0.95)], method='SLSQP',
-								args=(WEALTH[step][i], 
-									ALLOC[i], 
-									gamble_averages[community_membership[i]],
-									RISK[i], 
-									DEFAULT_A, 
-									DEFAULT_GAMMA, 
-									SAFE_RETURN)).x[0]
-				project_contributions[community_membership[i]] += \
-									np.sum(WEALTH[step][i]*ALLOC[i]*RISK[i]*(1-C[i])) 
+		# if multiprocess:
+		# 	args = [(WEALTH[step][i], ALLOC[i], 
+		# 			gamble_averages[community_membership[i]],
+		# 			RISK[i], DEFAULT_A, DEFAULT_GAMMA, SAFE_RETURN)
+		# 			for i in range(NUM_AGENTS)]
+		# 	with Pool() as pool:
+		# 		results = pool.map(optimize_utility, args)
+		# 	C = np.array(results)
+		# 	for i in range(NUM_AGENTS):
+		# 		project_contributions[community_membership[i]] += \
+		# 							np.sum(WEALTH[step][i]*ALLOC[i]*RISK[i]*(1-C[i]))  
+
+		# serial approach (better than multiprocessing for O(10^3) agents, but worse for >= O(10^4))
+		for i in range(NUM_AGENTS):
+			C[i] = minimize(utility, x0=0.5, bounds=[(0.05, 0.95)], method='SLSQP',
+							args=(WEALTH[step][i],
+								  AGENT_EXPECTED_RETURNS[i],
+								  DEFAULT_A,
+								  DEFAULT_GAMMA)).x[0]
+			project_contributions[community_membership[i]] += WEALTH[step][i](1-C[i])*ALLOC[i][:-1]
 		  
 
 		# run projects
@@ -226,20 +228,12 @@ def simulation(NUM_AGENTS=500, STEPS=50, SAFE_RETURN=1.1, DEFAULT_A=1.2, DEFAULT
 				gamble_success[idx] += 1
 	
 		for i in range(NUM_AGENTS):
-			# new wealth = old wealth - consumption - risky investment + risky return + safe return
 			I = 1-C[i]
-			S = 1-RISK[i]
-			agent_risky_return = np.array(risky_returns[community_membership[i]])
-			
-			# calculate income:
-			# sum of risky investment returns - risky investment amount ("-1") + safe return
-			INCOME[step][i] = sum(WEALTH[step][i]*I*RISK[i]*ALLOC[i]*(agent_risky_return-1)) + \
-								  WEALTH[step][i]*I*S*SAFE_RETURN
-			
-			# new wealth = current wealth - consumption + income from safe/risky investments
+			agent_returns = np.concatenate([np.array(risky_returns[community_membership[i]]), SAFE_RETURN])
+			INCOME[step][i] = sum(WEALTH[step][i]*I*ALLOC[i]*agent_returns)
 			WEALTH[step+1][i]  = WEALTH[step][i] - WEALTH[step][i]*C[i] + INCOME[step][i]
 
-	return WEALTH, INCOME, communities, RISK, gamble_success
+	return WEALTH, INCOME, communities, GAMMA_POS, gamble_success
 
 #################################################################################################
 
@@ -294,4 +288,15 @@ def fit_generalized_gamma(data):
 
 if __name__ == "__main__":
 
-	W, I, communities, risk, success = simulation(NUM_AGENTS=10000, STEPS=10, PROJECT_COST=0.5)
+	# parse command-line arguments
+	parser = argparse.ArgumentParser(description="Run ABM simulation.")
+	parser.add_argument("--agents", help="Number of agents", default=1000, type=int)
+	parser.add_argument("--steps", help="Number of steps", default=100, type=int)
+	parser.add_argument("--project-cost", help="Cost of starting a project", default=0.5, type=float)
+	args = parser.parse_args()
+
+	NUM_AGENTS   = args.agents
+	STEPS	  	 = args.steps
+	PROJECT_COST = args.project_cost
+	
+	W, I, communities, risk, success = simulation(NUM_AGENTS=NUM_AGENTS, STEPS=STEPS, PROJECT_COST=PROJECT_COST)
