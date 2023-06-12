49080903f1309e32608c17b9f90ebfe0412088c6
Charles Dupont <charles.aug.dupont@gmail.com>
2023-06-12 10:30:03 +0200

    add job script

---
+++ jobs/job.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Set job requirements
+
+#SBATCH -p thin
+#SBATCH -t 02:30:00
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=16
+
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=c.a.dupont@uva.nl
+
+# Load modules
+module load 2021
+module load Python/3.9.5-GCCcore-10.3.0
+pip install --user scipy
+pip install --user SALib
+pip install --user networkx
+pip install --user git+https://github.com/cvxgrp/cptopt.git
+
+
+# Create output directory on scratch
+mkdir "$TMPDIR"/output_directory &
+
+for i in `seq 0 15`; do
+	# Execute python script
+	python network_simulation.py "$TMPDIR"/output_directory $i &
+done
+wait
+
+# Copy output data to home directory
+cp -r "$TMPDIR"/output_directory $HOME
