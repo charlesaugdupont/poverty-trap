ac22f35141ff33257555e965397caa8f812d752e
Charles Dupont <charles.aug.dupont@gmail.com>
2023-07-31 13:04:07 +0200

    update sda network simulation script

--- network_simulation_sda.py
+++ network_simulation_sda.py
@@ -39,24 +39,19 @@ if __name__ == "__main__":
 
 	start_time = time.time()
 
-	# load a single set of graphs
-	with open("../sda_graphs.pickle", "rb") as f:
-		graphs = pickle.load(f)
-
-
 	# run each param combination
 	for iter_idx, row in enumerate(X):
 
 		# load graph corresponding to simga value of the current row
 		init_w_scale = row[4]
+		with open(f"../sda_graphs/{init_w_scale}.pickle", "rb") as f:
+			graph, communities, community_membership, initial_wealth = pickle.load(f)
 
 		# 10 repetitions to capture stochastic effects
 		for rep_idx, SEED in enumerate([1, 2, 3, 5, 8, 13, 21, 34, 55, 89]):
 		
-			graph, communities, community_membership, initial_wealth = graphs[row[4]]
-
 			W, A, R, P, T, _, G = simulation (
-				graph=graphs[4],
+				graph=graph,
 				communities=communities,
 				community_membership=community_membership,
 				NUM_AGENTS=1250,
