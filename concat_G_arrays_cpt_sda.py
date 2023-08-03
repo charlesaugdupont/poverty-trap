03eaab0b608da82bcb4b640b109e591044aafe26
Charles Dupont <charles.aug.dupont@gmail.com>
2023-08-03 12:25:27 +0200

    some updates

--- concat_G_arrays_cpt_sda.py
+++ concat_G_arrays_cpt_sda.py
@@ -15,16 +15,12 @@ if __name__ == "__main__":
 		data = pickle.load(lzma.open(f"../model_runs_cpt_sda_{SEED}/{f}"))
 		seed, param = f.split(".")[0].split("_")[:2]
 		G_arrays[seed][param] = data["G"].T
-	
-	num_params = len(G_arrays[seed])
-	num_projects = data["G"].T.shape[0]
-	num_steps = data["G"].T.shape[1]
 
 	for seed_idx, seed in enumerate(G_arrays):
-		G_SEED = np.zeros((num_params, num_projects, num_steps), dtype=np.float16)
+		G_SEED = []
 		param_G = G_arrays[seed]
 		keys = list(param_G.keys())
 		sorted_param_keys = sorted([int(x) for x in keys])
 		for k_idx, k in enumerate(sorted_param_keys):
-			G_SEED[k_idx] = param_G[str(k)]
-		pickle.dump(G_SEED, lzma.open(output_dir + f"/G_{SEED}_{num_params}_{num_projects}_{num_steps}_cpt_sda.pkl.lzma", "wb"))
+			G_SEED.append(param_G[str(k)])
+		pickle.dump(G_SEED, lzma.open(output_dir + f"/G_{SEED}_cpt_sda.pkl.lzma", "wb"))
--- concat_G_arrays_sda.py
+++ concat_G_arrays_sda.py
@@ -21,16 +21,12 @@ if __name__ == "__main__":
 		G_arrays[seed][param] = data["G"].T
 	print(f"{time.time()-start_time} : Finished loading data.", flush=True)
 
-	num_params = len(G_arrays[seed])
-	num_projects = data["G"].T.shape[0]
-	num_steps = data["G"].T.shape[1]
-
 	for seed_idx, seed in enumerate(G_arrays):
-		G_SEED = np.zeros((num_params, num_projects, num_steps), dtype=np.float16)
+		G_SEED = []
 		param_G = G_arrays[seed]
 		keys = list(param_G.keys())
 		sorted_param_keys = sorted([int(x) for x in keys])
 		for k_idx, k in enumerate(sorted_param_keys):
-			G_SEED[k_idx] = param_G[str(k)]
-		pickle.dump(G_SEED, lzma.open(output_dir + f"/G_{seed}_{num_params}_{num_projects}_{num_steps}_sda.pkl.lzma", "wb"))
+			G_SEED.append(param_G[str(k)])
+		pickle.dump(G_SEED, lzma.open(output_dir + f"/G_{seed}_sda.pkl.lzma", "wb"))
 		print(f"{time.time()-start_time} : Finished concatenating and storing seed {seed}", flush=True)
